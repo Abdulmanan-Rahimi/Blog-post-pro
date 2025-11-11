6e53c866c6dbacebaa988de194514a1141e1c18b
Abdulmanan-Rahimi <abdulmananrahimi5565@gmail.com>
2025-11-11 07:44:10 +0430

    managed the error handling when we add an article in db.json

--- src/pages/addArticle/AddArticle.jsx
+++ src/pages/addArticle/AddArticle.jsx
@@ -4,16 +4,47 @@ import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import "./AddArticle.css";
 import axios from "axios";
+import Swal from "sweetalert2";
 
 const AddArticle = () => {
   const [formData, setFormData] = useState({});
 
-  const addArticleHandler = () => {
-    axios.post("http://localhost:5000/articles", formData);
+  const resetFormData = () => {
+    setFormData({
+      title: "",
+      desc: "",
+      image: "",
+      category: "",
+      readingTime: "",
+      writter: "",
+    });
   };
 
-  const formHandler = (e, propertyName) => {
-    setFormData({ ...formData, [propertyName]: e.target.value });
+  const addArticleHandler = () => {
+    axios
+      .post("http://localhost:5000/articles", formData)
+      .then((Response) => {
+        if (Response.status === 201) {
+          Swal.fire({
+            title: "مقاله جدید با موفقیت اضافه شد",
+            timer: 1500,
+            timerProgressBar: true,
+          });
+        }
+      })
+      .catch((error) => {
+        Swal.fire({
+          title: "مقاله ساخته نشد",
+          timer: 1500,
+          icon: "error",
+          timerProgressBar: true,
+        });
+      });
+    resetFormData();
+  };
+
+  const formHandler = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
   return (
@@ -24,7 +55,9 @@ const AddArticle = () => {
           <Form.Group className="mb-3">
             <Form.Label>عنوان مقاله</Form.Label>
             <Form.Control
-              onChange={(e) => formHandler(e, "title")}
+              value={formData.title}
+              name="title"
+              onChange={formHandler}
               type="text"
               placeholder="عنوان مقاله را وارد کنید"
             />
@@ -33,7 +66,9 @@ const AddArticle = () => {
           <Form.Group className="mb-3">
             <Form.Label>توضیح کوتاه</Form.Label>
             <Form.Control
-              onChange={(e) => formHandler(e, "desc")}
+              value={formData.desc}
+              name="desc"
+              onChange={formHandler}
               type="text"
               placeholder="یه توضیح کوتاه در مورد مقاله وارد کنید"
             />
@@ -42,7 +77,9 @@ const AddArticle = () => {
           <Form.Group className="mb-3">
             <Form.Label>نویسنده مقاله</Form.Label>
             <Form.Control
-              onChange={(e) => formHandler(e, "writter")}
+              value={formData.writter}
+              name="writter"
+              onChange={formHandler}
               type="text"
               placeholder="نام نویسنده مقاله را وارد کنید"
             />
@@ -51,7 +88,9 @@ const AddArticle = () => {
           <Form.Group className="mb-3">
             <Form.Label>موضوع مقاله</Form.Label>
             <Form.Control
-              onChange={(e) => formHandler(e, "category")}
+              value={formData.category}
+              name="category"
+              onChange={formHandler}
               type="text"
               placeholder="موضوع مقاله را وارد کنید"
             />
@@ -60,7 +99,9 @@ const AddArticle = () => {
           <Form.Group className="mb-3">
             <Form.Label>عکس مقاله</Form.Label>
             <Form.Control
-              onChange={(e) => formHandler(e, "image")}
+              value={formData.image}
+              name="image"
+              onChange={formHandler}
               type="text"
               placeholder="عکس مقاله را وارد کنید"
             />
@@ -69,7 +110,9 @@ const AddArticle = () => {
           <Form.Group className="mb-3">
             <Form.Label>مدت زمان خواندن</Form.Label>
             <Form.Control
-              onChange={(e) => formHandler(e, "readingTime")}
+              value={formData.readingTime}
+              name="readingTime"
+              onChange={formHandler}
               type="number"
               placeholder=""
             />
